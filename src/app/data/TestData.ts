f176f6162e26e5b4e58fe6c8a6913525f70157bc
Evgeniy Gul <gulevgeniy@gmail.com>
2021-03-20 19:37:16 +0300

    dynamic sidebar & awesome fonts
    fix
    
    
    temp
    
    
    temp

--- src/app/data/TestData.ts
+++ src/app/data/TestData.ts
@@ -29,23 +29,26 @@ export class TestData {
     {
       id: 1,
       title: 'Заправить автомобиль бензином',
+      completed: false,
       priority: TestData.priorities[1],
       category: TestData.categories[9],
-      date: new Date('2021-04-10')
+      date: new Date('2021-04-10'),
     },
     {
       id: 2,
       title: 'Доделать отчет 15-31',
+      completed: false,
       priority: TestData.priorities[2],
       category: TestData.categories[0],
-      date: new Date('2021-04-11')
+      date: new Date('2021-04-11'),
     },
     {
       id: 3,
       title: 'Куптиь жене цветы',
+      completed: false,
       priority: TestData.priorities[3],
       category: TestData.categories[1],
-      date: new Date('2021-04-09')
+      date: new Date('2021-04-09'),
     },
   ];
 }
